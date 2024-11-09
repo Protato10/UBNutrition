dc9b450bfbf2874e177b30931e8a143a15693ea2
elianff <i.eliana.freeman@gmail.com>
2024-11-09 13:56:12 -0500

    updated GUI

--- UBNutrition/src/GUI.java
+++ UBNutrition/src/GUI.java
@@ -1,15 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+
+
+public class GUI implements ActionListener {
+
+
+    int count = 0;
+
+    private JPanel panel;
+
+    private JFrame frame;
+    private JLabel label;
+    private JTextField textField;
+    private JButton button1;
+    private JButton button2;
 
     public GUI(){
 
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        frame = new JFrame();
+        panel = new JPanel();
+        label = new JLabel("Number of clicks: 0");
+        textField = new JTextField();
+        button1 = new JButton("Enter");
+        button1.addActionListener(this);
 
-        panel.setBorder(BorderFactory.createEmptyBorder(50,50,10, 50));
+        panel.setBorder(BorderFactory.createEmptyBorder(100,100,100, 100));
         panel.setLayout(new GridLayout(0,1));
+        //panel.add(label);
+        //panel.add(textField);
+        panel.add(button1);
 
         frame.add(panel,BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,5 +43,11 @@ public class GUI {
 
     public static void main(String[] args) {
         new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " +count);
     }
 }
